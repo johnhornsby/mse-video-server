792f88f45018a18d546514f0947bb62a90d01255
John Hornsby <john@interactivelabs.co.uk>
2016-07-19 21:17:05 +0100

    socket io server added, video data not being passed through correctly

---
+++ app/index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('get video data', function (cb) {
+  	fs.readFile(__dirname + '/test.webm', 'binary', function(err, data) {
+  		if (err) {
+  			console.log(err);
+  		} else {
+  			cb(data);
+  		}
+  	});
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
--- public/app/index.js
+++ public/app/index.js
@@ -1,3 +1,6 @@
+import io from 'socket.io-client';
+
+
 const NUM_CHUNKS = 5;
 
 class Main {
@@ -10,12 +13,26 @@ class Main {
 
 	_chunkIndex = 0;
 
+	_data = null;
+
 	constructor() {
 		this._init();
 	}
 
 
 	_init() {
+		this._socket = io('http://localhost:3000');
+		this._socket.on('connect', ::this._onSocketConnect);
+	}
+
+
+	_onSocketConnect() {
+		this._socket.emit('get video data', ::this._onSocketGetVideoData);
+	}
+
+
+	_onSocketGetVideoData(data) {
+		this._data = data;
 
 		this._video = document.querySelector('video'); 
 
@@ -34,80 +51,17 @@ class Main {
 
 		console.log('mediaSource readyState: ' + this._mediaSource.readyState);
 
-		this._loadVideo('http://localhost:8080/public/test.webm', ::this._chunkVideo);
+		this._mediaSourceBuffer.appendBuffer(this._data);
+
+		if (this._video.paused) {
+			this._video.play(); // Start playing after 1st chunk is
+		}
+
+		this._mediaSourceBuffer.addEventListener('updateend', () => {
+			this._mediaSource.endOfStream();
+		});
 	}
 
-
-	_loadVideo(url, callback) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url, true);
-		xhr.responseType = 'arraybuffer';
-		xhr.send();
-
-		xhr.onload = function(e) {
-			if (xhr.status != 200) {
-				alert("Unexpected status code " + xhr.status + " for " + url);
-				return false;
-			}
-			callback(new Uint8Array(xhr.response));
-		};
-	}
-
-
-	_chunkVideo(uInt8Array) {
-
-		var file = new Blob([uInt8Array], {type: 'video/webm'});
-		var chunkSize = Math.ceil(file.size / NUM_CHUNKS);
-
-		console.log('num chunks:' + NUM_CHUNKS);
-		console.log('chunkSize:' + chunkSize + ', totalSize:' + file.size);
-
-		// Slice the video into NUM_CHUNKS and append each to the media element.
-		var i = 0;
-
-		var self = this;
-
-		this._readChunk(chunkSize, file);
-	}
-
-
-	_readChunk(chunkSize, file) {
-		var reader = new FileReader();
-
-		var startByte = chunkSize * this._chunkIndex;
-		var chunk = file.slice(startByte, startByte + chunkSize);
-
-		var self = this;
-
-		// Reads aren't guaranteed to finish in the same order they're started in,
-		// so we need to read + append the next chunk after the previous reader
-		// is done (onload is fired).
-		reader.onload = function(event) {
-			self._mediaSourceBuffer.appendBuffer(new Uint8Array(event.target.result));
-
-			console.log('appending chunk:' + self._chunkIndex);
-
-			if (self._chunkIndex == NUM_CHUNKS - 1) {
-				self._mediaSourceBuffer.addEventListener('updateend', () => {
-					self._mediaSource.endOfStream();
-				});
-
-			} else {
-				if (self._video.paused) {
-					self._video.play(); // Start playing after 1st chunk is appended.
-				}
-
-				self._chunkIndex += 1;
-				self._readChunk(chunkSize, file);
-			}
-		};
-
-		reader.readAsArrayBuffer(chunk);
-	}
-
-
-
-	
 
 
 }
